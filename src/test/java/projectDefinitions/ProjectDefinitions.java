be1021a6ad715856481c3b92e751ab0cdee1ef04
Marie <Marie.bjornberg@hotmail.com>
2021-03-25 14:24:06 +0100

    My 2 Commit

--- src/test/java/projectDefinitions/ProjectDefinitions.java
+++ src/test/java/projectDefinitions/ProjectDefinitions.java
@@ -12,22 +12,22 @@ public class ProjectDefinitions {
     }
 
     @Given("I have also selected a <username>")
-    public void i_have_also_selected_a_username() {
+    public void i_have_also_selected_a_username()throws InterruptedException {
 
     }
 
     @Given("I have also selected a <password>")
-    public void i_have_also_selected_a_password() {
+    public void i_have_also_selected_a_password()throws InterruptedException {
 
     }
 
     @When("I press sign up")
-    public void i_press_sign_up() {
+    public void i_press_sign_up() throws InterruptedException{
 
     }
 
     @Then("the <result> should be on the screen")
-    public void the_result_should_be_on_the_screen() {
+    public void the_result_should_be_on_the_screen()throws InterruptedException {
 
     }
 
